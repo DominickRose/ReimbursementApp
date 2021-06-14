dfa73ae113c0cdccebbb82fdc48d7f5675ca32f3
Dominick Rose <Dom.Rose42@gmail.com>
2021-06-14 14:35:43 -0600

    Refactored code so that javascript is in differnt files from HTML

---
+++ scripts/empScripts.js
@@ -0,0 +1,82 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const empId = urlSearchParams.get('id');
+
+const welcomeMessage = document.getElementById('welcomeMessage');
+const approvedAmount = document.getElementById('approvedAmount');
+const reimbursementTable = document.getElementById('reimbursementTable');
+const filterChoices = document.getElementById("filterChoices");
+const amountInput = document.getElementById('amountInput');
+const reasonInput = document.getElementById('descriptionInput');
+
+let reimbursements;
+
+async function initialize() {
+	const empBody = await fetchEmployees(empId);
+	welcomeMessage.innerHTML += empBody['firstName']
+
+	reimbursements = (await fetchReimbursements()).sort((first, second) => first.date - second.date);
+	populateTable();
+}
+
+async function populateTable() {
+	reimbursementTable.innerHTML = '';
+	if (filterChoices.value !== 'All') {
+		reimbursements.filter((reimbursement) => reimbursement.status == filterChoices.value).forEach(addToTable);
+	}
+	else {
+		reimbursements.forEach(addToTable);
+	}
+
+	approvedAmount.innerHTML = `$${calcSum(reimbursements, 'Approved')}`;
+}
+
+function createDelete(reimbursement) {
+	return reimbursement.status === 'Pending' ?
+		`<button class='btn-danger' id="delete_${reimbursement.reimbursementId}" onclick="deleteRequest(${reimbursement.reimbursementId})">Delete</button>` :
+		''
+}
+
+function addToTable(reimbursement) {
+	const date = new Date();
+	date.setTime(reimbursement.date)
+
+	reimbursementTable.innerHTML += `<tr><td>${date.getUTCMonth() + 1}/${date.getUTCDate()}/${date.getUTCFullYear()}</td><td>$${reimbursement.amount}</td><td>${reimbursement.reason}</td><td class="statusValue">${reimbursement.status}</td><td>${reimbursement.mgrMessage}</td><td>${createDelete(reimbursement)}</td></tr>`
+}
+
+async function submitRequest() {
+	const body = {
+		reimbursementId: 0,
+		ownerId: empId,
+		amount: amountInput.value,
+		reason: reasonInput.value,
+		status: 'Pending',
+		mgrMessage: '',
+		date: new Date().getTime()
+	}
+
+	const config = {
+		method: "POST",
+		headers: {'Content-Type':'application/json'},
+		body: JSON.stringify(body)
+	};
+
+	post_result = await fetch('http://localhost:5000/reimbursements', config);
+	if (post_result.status === 201) {
+		reimbursements = (await fetchReimbursements()).sort((first, second) => first.date - second.date);
+		populateTable();
+	}
+}
+
+async function deleteRequest(id) {
+	const config = {
+		method: "DELETE"
+	}
+	const shouldContinue = confirm("Are you sure you want to delete?");
+	if (shouldContinue) {
+		const result = await fetch(`http://localhost:5000/reimbursements/${id}`, config);
+		if (result.status == 205) {
+			reimbursements = (await fetchReimbursements()).sort((first, second) => first.date - second.date);
+			populateTable();
+		}
+	}
+}
---
+++ scripts/mgrScripts.js
@@ -0,0 +1,85 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const empId = urlSearchParams.get('id');
+
+const welcomeMessage = document.getElementById('welcomeMessage');
+const reimbursementTable = document.getElementById('reimbursementTable');
+const totalRequests = document.getElementById('totalRequests');
+const pendingRequests = document.getElementById('pendingRequests');
+
+const statusFilter = document.getElementById("statusFilter");
+const employeeFilter = document.getElementById('employeeFilter');
+
+const approvalCommentInput = document.getElementById('approvalCommentInput');
+
+let reimbursements;
+let employees;
+let activeReimbursement = 0;
+
+async function initialize() {
+	employees = await fetchEmployees();
+	welcomeMessage.innerHTML += employees[empId];
+
+	for(const employee in employees) {
+		employeeFilter.innerHTML += `<option value = ${employee}>${employees[employee]}</option>`
+	}
+
+	reimbursements = (await fetchReimbursements()).sort((first, second) => first.date - second.date);
+	populateTable();
+
+}
+
+async function populateTable() {
+	reimbursementTable.innerHTML = "";
+	if (statusFilter.value === "All" && employeeFilter.value === "All") {
+		reimbursements.forEach(addToTable);
+	}
+
+	else if (statusFilter.value === "All") {
+		reimbursements.filter((reimbursement) => reimbursement.ownerId == employeeFilter.value).forEach(addToTable);
+	}
+
+	else if (employeeFilter.value === "All") {
+		reimbursements.filter((reimbursement) => reimbursement.status == statusFilter.value).forEach(addToTable);
+	}
+	else {
+		reimbursements.filter((reimbursement) => reimbursement.status == statusFilter.value && reimbursement.ownerId == employeeFilter.value).forEach(addToTable);
+	}
+
+	pendingRequests.innerHTML = reimbursements.filter((reimbursement) => reimbursement.status == 'Pending').length
+	totalRequests.innerHTML = reimbursements.length
+}
+
+function addToTable(reimbursement) {
+	const date = new Date();
+	date.setTime(reimbursement.date)
+
+	reimbursementTable.innerHTML += `<tr><td>${date.getUTCMonth() + 1}/${date.getUTCDate()}/${date.getUTCFullYear()}</td><td>$${reimbursement.amount}</td><td>${employees[reimbursement.ownerId]}</td><td>${reimbursement.reason}</td><td class="reimbursement-status">${reimbursement.status}</td><td>${createButtons(reimbursement)}</td></tr>`
+}
+
+function createButtons(reimbursement) {
+	return reimbursement.status === 'Pending' ? 
+		`<button>Reject</button> <button class = 'approve-btn btn-primary' data-toggle="modal" data-target=#approveRequestModal onclick='activeReimbursement = ${reimbursement.reimbursementId}'>Approve</button>` : 
+		''
+}
+
+async function submitChange(action) {
+	const body = {
+		mgrMessage: approvalCommentInput.value,
+		status: action
+	};
+
+	approvalCommentInput.value = '';
+
+	const config = {
+		method: "PATCH",
+		headers: {'Content-Type': 'application/json'},
+		body: JSON.stringify(body)
+	};
+
+	response = await fetch(`http://localhost:5000/reimbursements/${activeReimbursement}/approve_or_deny`, config)
+
+	if (response.status == 200) {
+		reimbursements = (await fetchReimbursements()).sort((first, second) => first.date - second.date);
+		populateTable();
+	}
+}
